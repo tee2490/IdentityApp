67f263644a599e22fac0353baf430df053126e66
tee2490@hotmail.com <tee2490@hotmail.com>
2023-04-03 10:39:03 +0700

    Add project files.

---
+++ IdentityApp/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Data
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SqlExpress; Database=TestIdentityXX; Trusted_connection=true; TrustServerCertificate=true");
+        }
+
+        //สร้างข้อมูลเริ่มต้นให้กับ Role
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole { Name = "Member", NormalizedName = "MEMBER" },
+                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
+            );
+        }
+
+    }
+}
---
+++ IdentityApp/Program.cs
@@ -0,0 +1,73 @@
+﻿global using IdentityApp.Models;
+global using IdentityApp.Data;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddDbContext<DataContext>();
+
+#region Identityสร้างเซอร์วิส User,Role (ระวังการเรียงลำดับ)
+builder.Services.AddIdentityCore<ApplicationUser>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+}).AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+#endregion
+
+var app = builder.Build();
+
+#region  //สร้างฐานข้อมูลอัตโนมัติ
+using var scope = app.Services.CreateScope(); //using หลังทำงานเสร็จจะถูกทำลายจากMemory
+var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    await context.Database.MigrateAsync();   //สร้าง DB ให้อัตโนมัติถ้ายังไม่มี
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "Problem migrating data");
+}
+#endregion
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
